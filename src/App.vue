0ebb4223a11776600883a5869d32d23c1dc493f6
Hiroki <Nox.Lunaris@yaho.com>
2022-12-09 20:58:46 +0900

    Almond頼んだ

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
+
 <template>
-    <div>
-        <Homeview></Homeview>
-    </div>
+  <v-app>
+    <v-main>
+      <!-- 基本は v-main の中に記述するはず     -->
+      <!-- コンポーネントは htmlタグみたいに入れられる -->
+
+      <Homeview/>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Homeview from './views/HomeView.vue';
+import Homeview from './views/Homeview.vue';
 
 export default {
     name: 'App',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,37 @@
 <template>
-    <v-bottom-navigation
-        v-model="value"
-        :background-color="color"
-        dark
-        shift
-    >
-        <v-btn>
-            <span>Video</span>
+  <v-bottom-navigation
+    :value="value"
+    color="teal"
+    grow
+  >
+    <v-btn>
+      <span>Recents</span>
 
-            <v-icon>mdi-television-play</v-icon>
-        </v-btn>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
 
-        <v-btn>
-            <span>Music</span>
+    <v-btn>
+      <span>Favorites</span>
 
-            <v-icon>mdi-music-note</v-icon>
-        </v-btn>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
 
-        <v-btn>
-            <span>Book</span>
+    <v-btn>
+      <span>Nearby</span>
 
-            <v-icon>mdi-book</v-icon>
-        </v-btn>
-
-        <v-btn>
-            <span>Image</span>
-
-            <v-icon>mdi-image</v-icon>
-        </v-btn>
-    </v-bottom-navigation>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script>
-    export default {
-        data: () => ({ value: 1 }),
-
-        computed: {
-            color () {
-                switch (this.value) {
-                    case 0: return 'blue-grey'
-                    case 1: return 'teal'
-                    case 2: return 'brown'
-                    case 3: return 'indigo'
-                    default: return 'blue-grey'
-                }
-            },
-        },
-    }
+export default {
+  name: "HomePage",
+  components: {
+    data: () => ({ value: 1 }),
+  }
+}
 </script>
+
+<style scoped>
+</style>
