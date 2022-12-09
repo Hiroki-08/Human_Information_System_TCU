088e40306112c737a51fef0cffcb6551a293fafa
Hiroki <Nox.Lunaris@yaho.com>
2022-12-09 16:32:40 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
-  <hello-world />
+	<v-card style="width: 375px; height: 667px;">
+		<v-bottom-navigation color="primary" v-model="isActive" absolute="true">
+			<v-btn value="menu01">
+				<span>menu01</span>
+				<v-icon>mdi-history</v-icon>
+			</v-btn>
+
+			<v-btn value="menu02">
+				<span>menu02</span>
+				<v-icon>mdi-heart</v-icon>
+			</v-btn>
+
+			<v-btn value="menu03">
+				<span>menu03</span>
+				<v-icon>mdi-map-marker</v-icon>
+			</v-btn>
+		</v-bottom-navigation>
+	</v-card>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+	new Vue({
+		el: '#app',
+		vuetify: new Vuetify(),
+			data: {
+				isActive: 'menu01'
+			}
+		});
 </script>
+
