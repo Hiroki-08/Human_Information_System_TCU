e19380a9f3440525ca3a28fd5b0b83ae1c907175
Hiroki <Nox.Lunaris@yaho.com>
2022-12-09 18:55:41 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,34 @@
 <template>
-	<v-card style="width: 375px; height: 667px;">
-		<v-bottom-navigation color="primary" v-model="isActive" absolute="true">
-			<v-btn value="menu01">
-				<span>menu01</span>
-				<v-icon>mdi-history</v-icon>
-			</v-btn>
+  <v-card style="width: 375px; height: 667px;">
+    <v-bottom-navigation color="primary" v-model="isActive" absolute="true">
+      <v-btn value="menu01">
+        <span>menu01</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
 
-			<v-btn value="menu02">
-				<span>menu02</span>
-				<v-icon>mdi-heart</v-icon>
-			</v-btn>
-
-			<v-btn value="menu03">
-				<span>menu03</span>
-				<v-icon>mdi-map-marker</v-icon>
-			</v-btn>
-		</v-bottom-navigation>
-	</v-card>
+      <v-btn value="menu02">
+        <span>menu02</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      
+      <v-btn value="menu03">
+        <span>menu03</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-card>
 </template>
 
-<script>
-	new Vue({
-		el: '#app',
-		vuetify: new Vuetify(),
-			data: {
-				isActive: 'menu01'
-			}
-		});
-</script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
 
+<script>
+    new Vue({
+        el: '#app',
+        vuetify: new Vuetify(),
+        data: {
+            isActive: 'menu01',
+            toggle: true
+        }
+    });
+</script>
