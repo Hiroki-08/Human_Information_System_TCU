f3eea82d425afbf585530838cb02111773c398fc
Hiroki <Nox.Lunaris@yaho.com>
2022-12-10 10:49:13 +0900

    色変わらない３

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -1,28 +1,30 @@
 <template>
-  <v-bottom-navigation
-    :value="value"
-    color="orange"
-    grow
-  >
-    <v-btn>
-      <span>ホーム</span>
+  <v-app>
+    <v-bottom-navigation
+      :value="value"
+      color="orange"
+      grow
+    >
+      <v-btn>
+        <span>ホーム</span>
 
-      <v-icon>mdi-history</v-icon>
-    </v-btn>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
 
-    <v-btn>
-      <span>メニュー</span>
+      <v-btn>
+        <span>メニュー</span>
 
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
 
-    <v-btn>
-      <span>予約</span>
+      <v-btn>
+        <span>予約</span>
 
-      <v-icon>mdi-map-marker</v-icon>
-    </v-btn>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
 
-  </v-bottom-navigation>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
