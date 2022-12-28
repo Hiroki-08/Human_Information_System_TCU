f8717b1b8d325df911ee52d3d373225ff5f03314
Hiroki <Nox.Lunaris@yaho.com>
2022-12-28 23:27:06 +0900

    メニューバー強調

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -2,26 +2,27 @@
   <v-main>
     <v-bottom-navigation
       flat
-      color="orange"
-      background-color=""
+      color="white"
+      background-color="orange"
       min-width=256
       max-width=390px
+      height=64px
       fixed
       grow
     >
-      <v-btn to="/" value="home" color="white" x-large>
+      <v-btn to="/" value="home" color="orange" x-large height=64px >
         <span style="font-size: 10px">ホーム</span>
 
         <v-icon>mdi-home</v-icon>
       </v-btn>
 
-      <v-btn to="/menu" value="menu" color="white" x-large>
+      <v-btn to="/menu" value="menu" color="orange" x-large height=64px >
         <span style="font-size: 10px">メニュー</span>
 
         <v-icon>mdi-silverware-fork-knife</v-icon>
       </v-btn>
 
-      <v-btn to="/book" value="book" color="white" x-large>
+      <v-btn to="/book" value="book" color="orange" x-large height=64px >
         <span style="font-size: 10px">予約状況</span>
 
         <v-icon>mdi-checkbox-marked-circle-outline</v-icon>
